83e85c8cca6cdc1fd4b6637ae13d4b0cf13fc19f
Rodi Tountcheva <rodi.tountcheva@westrock.com>
2019-11-12 19:13:50 -0500

    replace with updated table creation script

--- SQL scripts/create data tables.sql
+++ SQL scripts/create data tables.sql
@@ -1,39 +1,36 @@
 CREATE TABLE "school_data" (
-    "School" text   NOT NULL,
-    "School_ID" int   NOT NULL,
-    "Board" text   NOT NULL,
-    "Board_ID" int   NOT NULL,
-    "Num_students" int   NOT NULL,
-    "Level_1" int   NOT NULL,
-    "Level_2" int   NOT NULL,
-    "Level_3" int   NOT NULL,
-    "Level_4" int   NOT NULL,
-    "Num_F" int   NOT NULL,
-    "Num_M" int   NOT NULL,
-    "Num_responses" int   NOT NULL,
-    CONSTRAINT "pk_school_data" PRIMARY KEY (
-        "School_ID"
-     )
+   "school" text   NOT NULL,
+   "school_id" int   NOT NULL,
+   "board" text   NOT NULL,
+   "board_id" int   NOT NULL,
+   "num_students" int   NOT NULL,
+   "level_1" int   NOT NULL,
+   "level_2" int   NOT NULL,
+   "level_3" int   NOT NULL,
+   "level_4" int   NOT NULL,
+   "num_f" int   NOT NULL,
+   "num_m" int   NOT NULL,
+   "num_responses" int   NOT NULL,
+   CONSTRAINT "pk_school_data" PRIMARY KEY (
+       "school_id"
+    )
 );
-
 CREATE TABLE "response_data" (
-    "School_ID" int   NOT NULL,
-    "Q1" int   NOT NULL,
-    "Q2" int   NOT NULL,
-    "Q3" int   NOT NULL,
-    "Q4" int   NOT NULL,
-    "Q5" int   NOT NULL,
-    "Q6" int   NOT NULL,
-    "Q7" int   NOT NULL,
-    "Q8" int   NOT NULL,
-    "Q9" int   NOT NULL,
-    "Q10" int   NOT NULL,
-    "Q11" int   NOT NULL,
-    CONSTRAINT "pk_response_data" PRIMARY KEY (
-        "School_ID"
-     )
+   "school_id" int   NOT NULL,
+   "q1" int   NOT NULL,
+   "q2" int   NOT NULL,
+   "q3" int   NOT NULL,
+   "q4" int   NOT NULL,
+   "q5" int   NOT NULL,
+   "q6" int   NOT NULL,
+   "q7" int   NOT NULL,
+   "q8" int   NOT NULL,
+   "q9" int   NOT NULL,
+   "q10" int   NOT NULL,
+   "q11" int   NOT NULL,
+   CONSTRAINT "pk_response_data" PRIMARY KEY (
+       "school_id"
+    )
 );
-
-ALTER TABLE "response_data" ADD CONSTRAINT "fk_response_data_School_ID" FOREIGN KEY("School_ID")
-REFERENCES "school_data" ("School_ID");
-
+ALTER TABLE "response_data" ADD CONSTRAINT "fk_response_data_school_id" FOREIGN KEY("school_id")
+REFERENCES "school_data" ("school_id");
